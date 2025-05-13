ec533dd5bfd6a53672124f3832d095c4aa9dae2b
Doğu Abaris <abaris@null.net>
2025-05-13 21:30:07 +0200

    Updates

---
+++ .svglintrc.js
@@ -0,0 +1,6 @@
+module.exports = {
+	plugins: [
+		'remove-title',
+		'remove-desc'
+	],
+};
---
+++ assets/js/aba21aa0.41b1b1d7.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunktelytics_docs=self.webpackChunktelytics_docs||[]).push([[742],{7093:s=>{s.exports=JSON.parse('{"name":"docusaurus-plugin-content-docs","id":"default"}')}}]);
